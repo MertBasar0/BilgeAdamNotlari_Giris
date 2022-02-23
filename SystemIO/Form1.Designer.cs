7f0f561d80a1587d25b563c3637b012ab5abb071
Mert <mertbasar0@hotmail.com>
2022-02-23 20:40:23 +0300

    23.02.2022-3

---
+++ SystemIO/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SystemIO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_systemIO2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_file = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_systemIO2
+            // 
+            this.btn_systemIO2.Location = new System.Drawing.Point(56, 32);
+            this.btn_systemIO2.Name = "btn_systemIO2";
+            this.btn_systemIO2.Size = new System.Drawing.Size(118, 24);
+            this.btn_systemIO2.TabIndex = 0;
+            this.btn_systemIO2.Text = "sistem2";
+            this.btn_systemIO2.UseVisualStyleBackColor = true;
+            this.btn_systemIO2.Click += new System.EventHandler(this.btn_systemIO2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(440, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(430, 404);
+            this.listBox1.TabIndex = 1;
+            // 
+            // btn_file
+            // 
+            this.btn_file.Location = new System.Drawing.Point(204, 32);
+            this.btn_file.Name = "btn_file";
+            this.btn_file.Size = new System.Drawing.Size(118, 24);
+            this.btn_file.TabIndex = 0;
+            this.btn_file.Text = "File";
+            this.btn_file.UseVisualStyleBackColor = true;
+            this.btn_file.Click += new System.EventHandler(this.btn_file_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 477);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_file);
+            this.Controls.Add(this.btn_systemIO2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_systemIO2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_file;
+    }
+}
+
---
+++ SystemIO/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemIO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Boolean tekCift(int sayi)
+        {
+            if (sayi % 2==0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        private void btn_systemIO2_Click(object sender, EventArgs e)
+        {
+            //bool sonuc = Directory.Exists(@"C:\\test2"); //Klasör var ise true, yok ise false döner.
+
+
+            //if (sonuc)
+            //{
+            //    Directory.Delete(@"C:\\test2"); //Klasör boş ise silecek.
+            //}
+            //else
+            //{
+            //    Directory.CreateDirectory(@"C:\\test2"); //boş klasör ekler..
+            //}
+
+            string[] dizi = Directory.GetDirectories(@"C:\\test2"); //Verilen yolda bulunan klasörleri getirir.
+
+            foreach (string d in dizi)
+            {
+                listBox1.Items.Add(d);
+            }
+
+            string[] dizii = Directory.GetFiles(@"C:\\test2");  //Verilen yoldaki bütün dosyaları getirir.
+
+            foreach (var item in dizii)
+            {
+                listBox1.Items.Add(item); //Verilen yoldaki dosyaları getirir.
+            }
+
+            string root = Directory.GetDirectoryRoot(@"C:\\test2");
+            MessageBox.Show(root); //Dosyanın ana dizini döner.
+
+            Directory.Move(@"C:\\test2", @"C:\\Ios"); // Var olan dosyayı silip verilen yolda bir dosya oluşturarak içeriği kopyalar.
+
+        }
+
+        private void btn_file_Click(object sender, EventArgs e)
+        {
+            // File: STATİC BİR DOSYA KONTROL SINIFIDIR. DIRECTORY İLE ÇOĞU AYNI METOTLARA SAHİP OLMASINA RAĞMEN
+            //file => dosyalarda .Directory =>> klasörlerde kullanılır..
+
+            #region Dosya Yazma ve oluşturma olayları
+
+            //FileStream fs = File.Create(@"C:\\Ios\\test.txt");
+            //MessageBox.Show(fs.Name);
+
+            //byte[] metin = new UTF8Encoding(true).GetBytes("merhaba arkadaşlar nasılsınız");
+
+            ////Dışarıdan oluşturuduğumuz yazıyı getbytes ile ascii kodları halinde dönüştürüyoruz ve byte dizisine atıyoruz. Yukarıda yazdığımız
+            ////utf8Encoding(true) ile Türkçe karakterlere destek vermesi için yazdık.
+            //fs.Write(metin, 0, metin.Length);
+            ////byte şeklinde yazmamızın nedeni white metodunun btyte dizisi istemesi.
+            //fs.Close(); // close satırı yazılmazsa yazma işlemi tamamlanmamış sayılacak ve işlem gerçekleşmeden program kapanacaktır.
+            #endregion
+
+            #region File
+
+            //List<string> vs = new List<string>();
+            //vs.Add("test");
+            //vs.Add("test2");
+
+            //File.AppendAllLines(@"C:\\Ios\\test.txt", vs, Encoding.UTF8);
+
+            ////Satırları bir dosyaya ekler ve sonra dosyayı kapatır. Belirtilen dosya mevcut değil ise, bu yöntem ile dosya oluşturur, satırları 
+            ////belirtilen dosyaya yazar ve sonra dosyayı kapatır.
+
+            //var result = from line in File.ReadLines(@"C:\\Ios\\test.txt")
+            //              where (line.StartsWith("test") || line.StartsWith("test2.txt"))
+            //              select line;
+            
+
+            //File.WriteAllLines(@"C:\\Ios\\test.txt", result);
+
+            //01.01.1700
+
+            DateTime dt = new DateTime(1700, 1, 1);
+            List<string> ks = new List<string>();
+
+            for (int i = 0; i < 500; i++)
+            {
+               DateTime yenizaman = dt.AddDays(i);
+               ks.Add(Convert.ToString(yenizaman));
+            }
+            
+
+            File.WriteAllLines(@"C:\\Ios\\test.txt", ks, Encoding.UTF8);
+
+            #endregion
+        }
+    }
+}
